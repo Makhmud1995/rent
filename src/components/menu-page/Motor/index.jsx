0691f72d95d5ba1d0bdf1732dbf0a21a30557b81
Makhmud1995 <yuldoshmakhmud@gmail.com>
2023-02-24 23:58:27 +0900

    camper

--- src/components/menu-page/Motor/index.jsx
+++ src/components/menu-page/Motor/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CarouselPage from '../../home/carousel'
 import MultiCarousel from '../../home/multi-carousel'
-import { Reccomend,Main,MotorPage } from './style'
+import { Reccomend,Main,MotorPage,Blog,Text,Img} from './style'
 import VideoPage from '../../home/Video/VideoPage'
 
 
@@ -15,6 +15,20 @@ const Motor = () => {
             <p>Video</p>
         </Main>
     <VideoPage/>
+    <Main>
+            <p>Blogs</p>
+        </Main>
+        <Blog>
+          <Text>
+<h2>Camper</h2>
+<h4>@camper_1</h4>
+<p>Camping truly is for everyone, from your oldest family member to little campers just trekking out for the first time. Whether your plan is to relax, explore or reconnect, CAMPER campgrounds are a great place to create memories with those you love.</p>
+
+          </Text>
+        <Img>
+        <VideoPage/>
+        </Img>
+        </Blog>
     </MotorPage>
   )
 }
--- src/components/menu-page/Motor/style.js
+++ src/components/menu-page/Motor/style.js
@@ -57,3 +57,73 @@ export const Main = styled.div`
     color: #373737;
     }
 `
+
+export const Blog = styled.div`
+width: 1226px;
+height: 605.95px;
+display: flex;
+justify-content: center;
+
+`
+
+export const Img= styled.div`
+width:50%;
+
+`
+export const Text = styled.div`
+width: 50%;
+display: flex;
+align-items: start;
+flex-direction: column;
+h2{
+    width: 116px;
+height: 34px;
+left: 70px;
+top: 2038.05px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 28px;
+line-height: 34px;
+
+color: #000000;
+}
+h4{
+    width: 104px;
+height: 22px;
+left: 70px;
+top: 2107.05px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 22px;
+/* identical to box height */
+padding-top: 15px;
+
+
+/* sariq */
+
+color: #FF7A00;
+
+}
+p{
+    width: 380px;
+height: 124px;
+left: 70px;
+top: 2139px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+
+color: #000000;
+padding-top: 15px;
+
+}
+
+`
